c282c573dc59a76b5bb99578127bd561f3d49a74
Yue Liu <liuer99cn@aliyun.com>
2016-11-15 20:30:38 +0800

    Update the message of email
    chore: Updating the message of feedback email, no production code change

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,7 +18,7 @@ $message = $_POST['message'];
 // create email body and send it	
 $to = 'liuyue@vliti.com'; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE FORM TO SEND MAIL TO*
 $email_subject = "申请DEMO演示:  $name";
-$email_body = "你从网站上收到一个客户的DEMO演示需求：\n\n"."下面是详情:\n\n姓名: $name\n\n邮箱: $email_address\n\n电话: $phone\n\n留言:\n$message";
+$email_body = "你从网站上收到一个客户的DEMO演示需求：\n\n"."下面是登记的情况:\n\n姓名: $name\n\n邮箱: $email_address\n\n电话: $phone\n\n留言:\n$message";
 $headers = "From: noreply@vliti.com\n"; // *REPLACE WITH THE EMAIL ADDRESS YOU WANT THE MESSAGE TO BE FROM*
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
